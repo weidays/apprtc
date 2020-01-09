ff4d5495fc4783f7f23836e133f24d563e695a3c
Jason <jason4working@gmail.com>
2020-01-09 16:44:48 +0800

    二次開發 不判斷滿了，直接替換

--- src/app_engine/constants.py
+++ src/app_engine/constants.py
@@ -61,7 +61,7 @@ WSS_INSTANCES = [{
 WSS_HOST_PORT_PAIRS = [ins[WSS_INSTANCE_HOST_KEY] for ins in WSS_INSTANCES]
 
 # memcache key for the active collider host.
-WSS_HOST_ACTIVE_HOST_KEY = 'wss_host_active_host'
+WSS_HOST_ACTIVE_HOST_KEY = 'apprtc.52buddha.com:8089'
 
 # Dictionary keys in the collider probing result.
 WSS_HOST_IS_UP_KEY = 'is_up'
